480291d27e2c81be48a23c42c5f03ea2c257c4b1
yomajo <ed.vinas@yahoo.com>
2023-01-17 17:08:39 +0200

    add second route

--- app.py
+++ app.py
@@ -10,6 +10,10 @@ def index():
     version = os.environ['VERSION']
     return f'app running version {version}'
 
+@app.route('/second')
+def index():
+    return f'second route'
+
 
 if __name__ == "__main__":
     pass
